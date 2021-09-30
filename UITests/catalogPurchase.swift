2b2c93b1f9a854a02798955b84568da029f8ca93
Georgios Romanas <giorgos.romanas@camelotls.com>
2021-09-30 17:36:03 +0300

    remove comment out steps

--- UITests/catalogPurchase.swift
+++ UITests/catalogPurchase.swift
@@ -9,26 +9,15 @@ import XCTest
 class CatalogPurchase : TestBase {
 
     func testProductCompleteOrder() throws {
-        
+
         let catalogScreen = CatalogScreen(app: app)
         let productDetailsScreen = ProductDetailsScreen(app: app)
-//        let myCartScreen = MyCartScreen(app: app)
-//        let loginScreen = LoginScreen(app: app)
-        
+
         catalogScreen.assertMoreButtonExists()
         catalogScreen.assertAllCatalogItemsText()
         catalogScreen.clickOnSpecificCatalogItem()
         productDetailsScreen.assertProductPriceExists()
         productDetailsScreen.assertAddToCartButtonExists()
         productDetailsScreen.clickAddToCartButton()
-//        productDetailsScreen.clickCartButton()
-//        myCartScreen.assertremoveItemExists()
-//        myCartScreen.myCartPriceExists()
-//        myCartScreen.proccedToCheckOutButtonExists()
-//        myCartScreen.clickProceedToCheckoutButton()
-//        loginScreen.asserLoginHeaderExists()
-//        loginScreen.assertuserNameLabelExists()
-//        loginScreen.assertpasswordLabelExists()
-//        loginScreen.assertloginButtonExists()
     }
 }
